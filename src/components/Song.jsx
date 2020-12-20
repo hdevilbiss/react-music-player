5ff846526979163ba26268b82b5e54c6eae345fa
hdevilbiss <hannahdevilbiss@gmail.com>
2020-12-20 18:25:06 -0500

    ✨ Create /components/Song: Show image, song, and artist

---
+++ src/components/Song.jsx
@@ -0,0 +1,17 @@
+const Song = () => {
+return (
+  <div className="song-container">
+    <p>
+      Song image
+    </p>
+    <p>
+      Song name
+    </p>
+    <p>
+      Song artists
+    </p>
+  </div>
+)
+}
+
+export default Song;
