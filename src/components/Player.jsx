9f65aa7e1824d43f53cae752836cda1b409a7c6a
hdevilbiss <hannahdevilbiss@gmail.com>
2020-12-20 18:47:25 -0500

    🎨 Add start, rewind, and fast forward icons to Player

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlayCircle, faAngleDoubleLeft, faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
+
 const Player = () => {
   return (
     <div className="player">
@@ -14,9 +17,9 @@ const Player = () => {
         </p>
       </div>
       <div className="play-control">
-        <p>
-          Icons
-        </p>
+        <FontAwesomeIcon className="rewind" icon={faAngleDoubleLeft} />
+        <FontAwesomeIcon className="play" icon={faPlayCircle} />
+        <FontAwesomeIcon className="fast-forward" icon={faAngleDoubleRight} />
       </div>
     </div>
   )
