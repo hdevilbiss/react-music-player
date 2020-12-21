5554830c6c20462a053280791c8e586cc4e83eb6
hdevilbiss <hannahdevilbiss@gmail.com>
2020-12-20 19:28:26 -0500

    🧐 Import main style and the new song data

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,16 @@
 import Player from './components/Player';
 import Song from './components/Song';
 
+/**
+ * Import styles
+ */
+import "./styles/app.scss";
+
+/**
+ * Import song data
+ */
+import data from './data';
+
 function App() {
   return (
     <div className="App">
