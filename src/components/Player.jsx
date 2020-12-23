ff38a8361027959bccc86ccfa66308923a6641f7
Hannah <45151652+shed-00@users.noreply.github.com>
2020-12-23 17:55:25 -0500

    ✨ Add audioRef and button handler to Player

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,7 +1,23 @@
+import {useRef} from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlayCircle, faAngleDoubleLeft, faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
 
-const Player = () => {
+const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
+  /**
+   * References
+   */
+  const audioRef = useRef(null);
+
+  /**
+   * Event Handlers
+   */
+  const playSongHandler = () => {
+    isPlaying
+      ? audioRef.current.pause()
+      : audioRef.current.play();
+      setIsPlaying(!isPlaying);
+  }
+
   return (
     <div className="player">
       <div className="duration-display">
@@ -15,9 +31,10 @@ const Player = () => {
       </div>
       <div className="play-control">
         <FontAwesomeIcon className="rewind" icon={faAngleDoubleLeft} size="2x" />
-        <FontAwesomeIcon className="play" icon={faPlayCircle} size="2x" />
+        <FontAwesomeIcon onClick={playSongHandler} className="play" icon={faPlayCircle} size="2x" />
         <FontAwesomeIcon className="fast-forward" icon={faAngleDoubleRight} size="2x" />
       </div>
+      <audio ref={audioRef} src={currentSong.audio}></audio>
     </div>
   )
   }
