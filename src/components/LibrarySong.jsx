e728e3cf9e6f336a70aeb688d72df8329221928d
Hannah <45151652+shed-00@users.noreply.github.com>
2020-12-24 15:30:31 -0500

    ✨ Create LibrarySong to show many songs from data.js

---
+++ src/components/LibrarySong.jsx
@@ -0,0 +1,13 @@
+const LibrarySong = ({songs}) => {
+  return (
+    <div className="song-container">
+    <img src="" alt="cover" />
+    <h2>
+      Name
+    </h2>
+    <h3>
+      Artist
+    </h3>
+  </div>
+  )
+}
