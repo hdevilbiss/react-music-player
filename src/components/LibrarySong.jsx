a0f8433e138db7779fb102aface0f04c8ce5e366
hdevilbiss <hannahdevilbiss@gmail.com>
2020-12-28 16:34:20 -0500

    ✨ Add songSelectHandler to the library-song onClick event

--- src/components/LibrarySong.jsx
+++ src/components/LibrarySong.jsx
@@ -1,6 +1,14 @@
-const LibrarySong = ({ song }) => {
+
+const LibrarySong = ({ song, setCurrentSong, songs, id }) => {
+  /**
+   * Event Handlers
+   */
+  const songSelectHandler = (event) => {
+    setCurrentSong(song);
+  }
+
   return (
-    <div className="library-song">
+    <div onClick={songSelectHandler} className="library-song">
     <img src={song.cover} alt={song.name} />
     <div className="song-description">
       <h3>
