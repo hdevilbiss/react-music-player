5fbe82da0d14ae6ae8643d38588176216d5ffd87
hdevilbiss <hannahdevilbiss@gmail.com>
2020-12-29 20:42:03 -0500

    ✨ Add FF and RW features to Player using currentSong, songs, and setCurrentSong props

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,7 +1,8 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlayCircle, faPauseCircle, faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import { useState} from "react";
 
-const Player = ({ currentSong, isPlaying, setIsPlaying, audioRef, songInfo, setSongInfo }) => {
+const Player = ({ currentSong, setCurrentSong, isPlaying, setIsPlaying, audioRef, songInfo, setSongInfo, songs }) => {
   /**
    * getTime function
    * format time in MM:SS
@@ -26,6 +27,18 @@ const Player = ({ currentSong, isPlaying, setIsPlaying, audioRef, songInfo, setS
     audioRef.current.currentTime = event.target.value;
     setSongInfo({...songInfo, currentTime: event.target.value});
   }
+  const skipHandler = (direction) => {
+    const curIdx = songs.indexOf(currentSong);
+    let newIdx = 0;
+    direction === 'forward'
+      ? curIdx === songs.length - 1
+        ? newIdx = 0
+        : newIdx = curIdx + 1
+      : curIdx === 0
+        ? newIdx = songs.length - 1
+        : newIdx = curIdx - 1;
+    setCurrentSong(songs[newIdx]);
+  }
 
   /**
    * Return
@@ -36,20 +49,36 @@ const Player = ({ currentSong, isPlaying, setIsPlaying, audioRef, songInfo, setS
         <p>
           {getTime(songInfo.currentTime)}
         </p>
-        <input min={0} max={songInfo.duration || 0} value={songInfo.currentTime} type="range" onChange={dragHandler} />
+        <input
+          min={0}
+          max={songInfo.duration || 0}
+          value={songInfo.currentTime}
+          type="range"
+          onChange={dragHandler}
+        />
         <p>
           {getTime(songInfo.duration)}
         </p>
       </div>
       <div className="play-control">
-        <FontAwesomeIcon className="rewind" icon={faAngleLeft} size="2x" />
+        <FontAwesomeIcon
+          className="rewind"
+          onClick={() => skipHandler('rewind')}
+          icon={faAngleLeft}
+          size="2x"
+        />
         <FontAwesomeIcon
           onClick={playSongHandler}
           className="play"
           icon={isPlaying ? faPauseCircle : faPlayCircle}
+          size="3x"
+        />
+        <FontAwesomeIcon
+          className="fast-forward"
+          onClick={() => skipHandler('forward')}
+          icon={faAngleRight}
           size="2x"
         />
-        <FontAwesomeIcon className="fast-forward" icon={faAngleRight} size="2x" />
       </div>
     </div>
   )
